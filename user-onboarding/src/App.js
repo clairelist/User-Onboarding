ba314586ea5f4c75dc815a0e47c36305b3dd730a
Claire List <claire.list2501@gmail.com>
2021-11-10 17:18:38 -0700

    form created need validation and submit

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,12 +1,33 @@
 //DATA / IMPORT SECTION
 import './App.css';
 
+import React, {useState} from 'react';
 import Form from './Form';
 
+const initialFormValues = {
+  username:'',
+  password:'',
+  email:'',
+  tos:0 //NOT AN INT! this is a boolean!!!
+}
+
 function App() {
+
+  const [formValues,setFormValues] = useState(initialFormValues);
+
+  const handleSubmit = () => {
+    //TO BE BUILT OUT!
+  }
+
+  const handleChange = (name,value) => {
+    setFormValues({...formValues,[name]:value});
+  }
+
   return (
     <div className="App">
      <h1>THIS ISTHE START</h1>
+     <p>I am a paragrpha</p>
+     <Form values={formValues} change={handleChange}/>
     </div>
   );
 }
---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,72 @@
+//DATA / IMPORT SECTION
+
+import React from 'react';
+
+//LOGIC / FUNCTION SECTION
+
+const Form = (props) => {
+
+    //'DATA' SECTION
+
+    const {username,email,password,tos} = props.values; //since we will have change handler funcy on plain props value
+
+    const {change, submit} = props;
+
+    //'LOGIC' AKA 'FUNCTION' SECTION
+
+    const onChange = (event) => {
+       const {name,value,checked,type} = event.target;
+       const checkVal = type === 'checkbox' ? checked : value;
+       change(name,checkVal);
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault(); //must prevent page from reloading as is 'default behaviour'
+        submit();
+    }
+
+    //'RENDER' SECTION
+    return (
+        <div>
+        <h1>WELCOME, USERS.</h1>
+        <form onSubmit={onSubmit}>
+            <lable>NAME:
+                <input 
+                    type='text'
+                    name='username'
+                    value={username}
+                    onChange={onChange}
+                />
+            </lable>
+            <lable>EMAIL:
+                <input 
+                    type='email'
+                    name='email'
+                    value={email}
+                    onChange={onChange}
+                />
+            </lable>
+            <lable>PASSWORD:
+                <input 
+                    type='password'
+                    name='password'
+                    value={password}
+                    onChange={onChange}
+                />
+            </lable>
+            <lable>TERMS of SERVICE:
+                <input 
+                    type='checkbox'
+                    name='tos'
+                    checked={tos}
+                    onChange={onChange}
+                />
+            </lable>
+        <input type='submit' value='CREATE a USER' />
+        </form>
+        </div>
+       
+    )
+}
+
+export default Form;
